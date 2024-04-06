970abd5a582e86e39176b25b1fa3c688612f40d5
Kowshiksai <kowshik.sai98@gmail.com>
2024-04-06 02:30:57 -0400

    NavToggle

--- Portfolio Website/src/components/header/Header.jsx
+++ Portfolio Website/src/components/header/Header.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './header.css';
 
 const Header = () => { 
+    const[Toggle, showMenu] = useState(false);
+
     return (
         <header className='header'>
         <nav className='nav container'>
             <a href="index.html" className='nav_logo'>Kowshik</a>
-            <div className='nav__menu'>
+            <div className={Toggle ? 'nav__menu show-menu' : 'nav__menu'}>
                 <ul className='nav__list grid'>
                     <li className='nav__item'>
-                        <a href="#about" className='nav__link'>
+                        <a href="#about" className='nav__link active-link'>
                             <i className='uil uil-estate nav__icon'></i> Home
                             </a>
                     </li>
@@ -33,6 +36,11 @@ const Header = () => {
                             </a>
                     </li> 
                 </ul>
+                <i class="uil uil-times nav__close"  onClick={() => showMenu(!Toggle)}></i>
+            </div>
+
+            <div className='nav__toggle' onClick={() => showMenu(!Toggle)}>
+                <i class="uil uil-apps nav__close"></i>
             </div>
         </nav>
         </header>
