0a478a414dc79b3c8983a18f0b28c6ee7e784536
Kowshiksai <kowshik.sai98@gmail.com>
2024-05-03 00:18:55 -0400

    Contact module

--- Portfolio Website/src/App.jsx
+++ Portfolio Website/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Home from './components/home/Home.jsx';
 import About from './components/about/About.jsx';
 import Skills from './components/skills/Skills.jsx';
+import Contact from './components/contact/Contact.jsx';
 
 const App = () => {
 
@@ -15,6 +16,7 @@ const App = () => {
         <Home />
         <About />
         <Skills />
+        <Contact />
       </main>
     </>
   );
